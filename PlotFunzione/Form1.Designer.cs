0d7734be8086d2f814bfb07e23178e4ce9359149
ballestrazzi <ballestrazzi@INF3-CATT>
2023-10-20 09:30:40 +0200

    Aggiungere i file di progetto.

---
+++ PlotFunzione/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PlotFunzione
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlot = new System.Windows.Forms.Button();
+            this.txtXMin = new System.Windows.Forms.TextBox();
+            this.txtXMax = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPlot
+            // 
+            this.btnPlot.Location = new System.Drawing.Point(12, 12);
+            this.btnPlot.Name = "btnPlot";
+            this.btnPlot.Size = new System.Drawing.Size(75, 23);
+            this.btnPlot.TabIndex = 0;
+            this.btnPlot.Text = "Plot";
+            this.btnPlot.UseVisualStyleBackColor = true;
+            this.btnPlot.Click += new System.EventHandler(this.btnPlot_Click);
+            // 
+            // txtXMin
+            // 
+            this.txtXMin.Location = new System.Drawing.Point(121, 15);
+            this.txtXMin.Name = "txtXMin";
+            this.txtXMin.Size = new System.Drawing.Size(100, 20);
+            this.txtXMin.TabIndex = 1;
+            this.txtXMin.Text = "0";
+            // 
+            // txtXMax
+            // 
+            this.txtXMax.Location = new System.Drawing.Point(238, 15);
+            this.txtXMax.Name = "txtXMax";
+            this.txtXMax.Size = new System.Drawing.Size(100, 20);
+            this.txtXMax.TabIndex = 2;
+            this.txtXMax.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtXMax);
+            this.Controls.Add(this.txtXMin);
+            this.Controls.Add(this.btnPlot);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlot;
+        private System.Windows.Forms.TextBox txtXMin;
+        private System.Windows.Forms.TextBox txtXMax;
+    }
+}
+
---
+++ PlotFunzione/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PlotFunzione
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlot_Click(object sender, EventArgs e)
+        {
+            double xMin = double.Parse(txtXMin.Text);
+            double xMax = double.Parse(txtXMax.Text);
+
+            double rangeDominio = xMax - xMin;
+            const double rangeCodominio = 2;
+
+            int widthSchermo = ClientSize.Width - 20;
+            int heightSchermo = ClientSize.Height - 10;
+
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+
+            int xSchermoPrecedente = int.MinValue;
+            int ySchermoPrecedente = int.MinValue;
+
+            for (double x = xMin; x < xMax; x += rangeDominio / 3 / widthSchermo)
+            {
+                double y = Function(x);
+                int xSchermo = (int)((x - xMin) * widthSchermo / rangeDominio);
+                int ySchermo = (int)((y + 1) * heightSchermo / rangeCodominio);
+
+                if (ySchermoPrecedente != int.MinValue)
+                {
+                    g.DrawLine(Pens.Black, xSchermoPrecedente, ySchermoPrecedente, xSchermo, ySchermo);
+                }
+
+                xSchermoPrecedente = xSchermo;
+                ySchermoPrecedente = ySchermo;
+                // g.FillEllipse(Brushes.Black, xSchermo, ySchermo, 2, 2);
+            }
+        }
+
+        private static double Function(double x)
+        {
+            return x * x*x * Math.Sin(10 * x);
+        }
+
+    }
+}
